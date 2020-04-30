c527f73736ca905974e36a44d5038d192cd75289
Sean Yeo <seansqyeo@gmail.com>
2020-04-30 10:44:43 -0700

    resize zero and decimal button

---
+++ script.js
@@ -0,0 +1,18 @@
+// add event listeners to all buttons on click
+
+//for now, make it so the text on button being clicked
+//resets the text currently on the display
+
+//function for numbers
+
+//function for decimal
+
+//function for resetters
+
+//function for mathematical operators
+
+// function addEventListeners() {
+//     let allButtons = document.querySelectorAll("button");
+//     allButtons.addEventListeners("click",
+//     e => e.target);
+// }
