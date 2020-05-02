2086ce99bf775ea8d678b5c998463e0044a03d11
Sean Yeo <seansqyeo@gmail.com>
2020-05-02 15:12:22 -0700

    do not allow inputs with more than 15 digits

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ function operandDisplay(e) {
         if (displayArray.length === 0 && e.target.textContent !== "0") {
             displayArray.push(e.target.textContent);
             display.textContent = displayArray.join("");
-        } else if (displayArray.length > 0) {
+        } else if (displayArray.length > 0 && displayArray.length < 15) {
             displayArray.push(e.target.textContent);
             display.textContent = displayArray.join("");
         }
